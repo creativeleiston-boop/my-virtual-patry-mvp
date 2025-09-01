06135b656ede02df7d201e38d6be68bcfae39c16
creativeleiston-boop <creativeleiston@gmail.com>
2025-09-01 11:35:07 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from 'lib/supabaseClient';
+
 
 export default function Home() {
   const [orgs, setOrgs] = useState(null);
